e543bc9062b65cdeb9195c995e7fefce68838cd2
Pothabattula Vinod <83913882+pothabattulavinod@users.noreply.github.com>
2025-09-25 19:56:55 +0530

    Create movie.js

---
+++ movie.js
@@ -0,0 +1,29 @@
+ window.movies = [
+  { 
+    title: "Hi Nanna", 
+    url: "hinanna.html", 
+    date: "2023-07-21",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3QCTJgrdACL3SX4Y-SUi4kB_nJVKJtGqNr-PPemRhNw&s=10"
+  },
+  { 
+    title: "Pushpa 2: The Rule", 
+    url: "pushpa2.html", 
+    date: "2025-09-20",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4WXrgDKE21Bh5vBiquqoBYo1SINe3kem8g3YYG7HIRw&s=10"
+  },
+  // Manual new movies with badge
+  { 
+    title: "Mirai", 
+    url: "mirai.html", 
+    date: "2025-09-24", 
+    isNew: true,
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAr-jxaZcAKqOt3O67coaD5X9ie_ABH_HBIwvzEx5RcA&s=10"
+  },
+  { 
+    title: "They Call Him OG", 
+    url: "og.html", 
+    date: "2025-09-25", 
+    isNew: true,
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdSZE1333EkF8-5y4Ja4UHji81Kbv4oBaQI25locVVqw&s=10"
+  }
+];
