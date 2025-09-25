53fe464c998cf4dd13783761f78c66e2270ce511
Pothabattula Vinod <83913882+pothabattulavinod@users.noreply.github.com>
2025-09-25 12:38:45 +0530

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,11 @@
+export const movies = [
+  { title: "Inception", url: "#", date: "2010-07-16" },
+  { title: "Interstellar", url: "#", date: "2014-11-07" },
+  { title: "Tenet", url: "#", date: "2020-08-26" },
+  { title: "Avatar: The Way of Water", url: "#", date: "2022-12-16" },
+  { title: "Oppenheimer", url: "#", date: "2023-07-21" },
+  { title: "New Sci-Fi Movie", url: "#", date: "2025-09-25" },
+  // Manual new movies with badge
+  { title: "Manual New Sci-Fi", url: "#", date: "2025-09-20", isNew: true },
+  { title: "Exclusive New Thriller", url: "#", date: "2025-09-22", isNew: true }
+];
