683a11d90237c3f6f6d74536fd8c6d5b5ad5a005
Pothabattula Vinod <83913882+pothabattulavinod@users.noreply.github.com>
2025-09-26 21:50:12 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,9 +1,36 @@
 export const movies = [
+{ 
+    title: "Mahavatar Narsimha", 
+    url: "mn.html", 
+    date: "2025-07-25",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiBTgX9kipfKQd9kFUzH4MeEmUeTKXqDcQd2vA3BlEUQ&s=10"
+},
+
+  { 
+    title: "Kaithi", 
+    url: "kaithi.html", 
+    date: "2019-10-25",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDJU-bT48BfYhCSkvEgtpWt9plkSGBEEFqmUC3aPqgvA&s=10"
+},
+
+  { 
+    title: "Court - State Vs. A Nobody", 
+    url: "court.html", 
+    date: "2025-03-14",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRklKDKY8z5-FCqh5XMgvYbkOHv3UhHE2AXlimxH0TmXA&s=10"
+},
+
+{ 
+    title: "Dasara", 
+    url: "dasara.html", 
+    date: "2023-03-30",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5JLgUeEnjOeOYsy4Z6reKGycazgHBu34AaTiikWmylA&s=10"
+},
   
  { 
     title: "Little Hearts", 
     url: "lh.html", 
-    date: "2025-09-24",
+    date: "2025-09-05",
     poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC4Le2yAHqP2Pz7neVXQ7GGIJHdau3pimK9yoVT9h6scM0NRwMlTANSFo&s"
   }
 ,
@@ -17,14 +44,14 @@ export const movies = [
   { 
     title: "Pushpa 2: The Rule", 
     url: "pushpa2.html", 
-    date: "2025-09-20",
+    date: "2024-12-05",
     poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4WXrgDKE21Bh5vBiquqoBYo1SINe3kem8g3YYG7HIRw&s=10"
   },
   // Manual new movies with badge
   { 
     title: "Mirai", 
     url: "mirai.html", 
-    date: "2025-09-24", 
+    date: "2025-09-05", 
     isNew: true,
     poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAr-jxaZcAKqOt3O67coaD5X9ie_ABH_HBIwvzEx5RcA&s=10"
   },
