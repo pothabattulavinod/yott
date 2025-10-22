1b598141f36f49c3ef449f707561f57bdba8a06d
Pothabattula Vinod <83913882+pothabattulavinod@users.noreply.github.com>
2025-10-22 20:49:22 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -3,7 +3,7 @@ export const movies = [
 
 { 
     title: "Baahubali 1 & 2", 
-    url: "avg/baahubali.html", 
+    url: "m/baahubali.html", 
     date: "2017-04-28",
     poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmCYPdN5BMWaiGzFpfevnJEKG3uC8tsn1_-zFrOdQP6g&s=10"
 },
